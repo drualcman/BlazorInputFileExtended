373aadcce1836bb037b416293d96d0ff76c902e2
Sergi Ortiz Gómez <drualcman@msn.com>
2021-07-05 19:47:52 +0800

    Prepare a demo for upload one file

---
+++ Website/Client/Pages/demos/Single.razor.cs
@@ -0,0 +1,37 @@
+﻿using BlazorInputFileExtended;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Website.Client.Pages.demos
+{
+    public partial class Single
+    {
+        string ErrorsMessage;
+        string UploadMessage;
+        string CompletedMessage;
+        string SaveMessage;
+
+        InputFileExtended<bool> UploadImage;
+
+        async Task Save()
+        {
+            SaveMessage = "Uploading image ... (Simulate 3 seconds)";
+            await Task.Delay(3000);
+            SaveMessage = "Image Uploaded!";
+            UploadImage.FileBytes = null;
+
+        }
+
+        void Error(ArgumentException e) => ErrorsMessage = e.Message;
+
+        void UploadFile(FileUploadEventArgs e) =>
+            UploadMessage = $"File name: {e.File.Name} File type: {e.File.ContentType} Size: {e.File.Size} Action: {e.Action}";
+
+        void Completed(FilesUploadEventArgs e) =>
+            CompletedMessage = $". Files loadted: {e.Count} with total size: {e.Size} Action: {e.Action}";
+    }
+}
---
+++ Website/Server/Controllers/FilesController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Website.Server.Controllers
+{
+    [Route("files")]
+    [ApiController]
+    public class FilesController : ControllerBase
+    {
+        private readonly IWebHostEnvironment Env;
+        public FilesController(IWebHostEnvironment env)
+        {
+            Env = env;
+        }
+
+        [HttpPost]
+        public async Task<bool> Disco([FromForm] IEnumerable<IFormFile> files)
+        {            
+            return true;
+        }
+    }
+}
