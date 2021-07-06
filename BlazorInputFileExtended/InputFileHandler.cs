b483a9dda9aa661c2bb84208a4b66feb7d0471d7
Sergi Ortiz Gómez <drualcman@msn.com>
2021-07-06 11:15:20 +0800

    add methods to manage the events from a derivated class can be overwrite also

--- BlazorInputFileExtended/InputFileHandler.cs
+++ BlazorInputFileExtended/InputFileHandler.cs
@@ -136,22 +136,77 @@ namespace BlazorInputFileExtended
         /// <summary>
         /// Event to notify each file uploaded
         /// </summary>
-        public virtual event UploadEventHandler OnUploadFile;
+        public event UploadEventHandler OnUploadFile;
 
         /// <summary>
         /// Event to notify all files are uploaded
         /// </summary>
-        public virtual event UploadsEventHandler OnUploaded;
+        public event UploadsEventHandler OnUploaded;
 
         /// <summary>
         /// Event to notify errors occurs
         /// </summary>
-        public virtual event UploadErrorEventHandler OnUploadError;
+        public event UploadErrorEventHandler OnUploadError;
 
         /// <summary>
         /// Event to notify errors occurs uploading to the api
         /// </summary>
-        public virtual event APIErrorEventHandler OnAPIError;
+        public event APIErrorEventHandler OnAPIError;
+
+        #region expose events to derivated classes
+        /// <summary>
+        /// Trigger the OnUploadFile from a derivated class
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        protected virtual void OnUploadFileEvent(object sender, FileUploadEventArgs e)
+        {
+            if (OnUploadFile is not null)
+            {
+                OnUploadFile(sender, e);
+            }
+        }
+
+        /// <summary>
+        /// Trigger the OnUploaded from a derivated class
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        protected virtual void OnUploadedEvent(object sender, FilesUploadEventArgs e)
+        {
+            if (OnUploaded is not null)
+            {
+                OnUploaded(sender, e);
+            }
+        }
+
+        /// <summary>
+        /// Trigger the OnUploadError from a derivated class
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        protected virtual void OnUploadErrorEvent(object sender, ArgumentException e)
+        {
+            if (OnUploadError is not null)
+            {
+                OnUploadError(sender, e);
+            }
+        }
+
+        /// <summary>
+        /// Trigger the OnAPIError from a derivated class
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        protected virtual void OnAPIErrorEvent(object sender, ArgumentException e)
+        {
+            if (OnAPIError is not null)
+            {
+                OnAPIError(sender, e);
+            }
+        }
+        #endregion
+
         #endregion
 
         #region methods to manage files
