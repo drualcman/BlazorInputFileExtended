f351688f73928d763bf14bb10e9accf222fd0dee
Sergi Ortiz Gómez <drualcman@msn.com>
2021-07-07 19:19:23 +0800

    add how to change buttons add how to use handler add new menus

---
+++ Website/Client/Pages/howto/Handler.razor.cs
@@ -0,0 +1,61 @@
+﻿using BlazorInputFileExtended;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Website.Client.Pages.howto
+{
+    public partial class Handler
+    {
+        [Inject]
+        public HttpClient Client { get; set; }
+
+        InputFileHandler FileHandler;
+        string Messages;
+
+        protected override void OnInitialized()
+        {
+            FileHandler = new InputFileHandler(Client);
+            FileHandler.OnUploadError += FileHandler_OnUploadError;
+            FileHandler.OnUploadFile += FileHandler_OnUploadFile;            
+        }
+
+        #region InputFile
+        private void FileHandler_OnUploadFile(object sender, FileUploadEventArgs e)
+        {
+            FileHandler.Add(e.File);
+            Messages = $"File {e.File.Name} chosen. Total size {e.File.Size}.";
+        }
+
+        private void FileHandler_OnUploadError(object sender, ArgumentException e)
+        {
+            Messages = $"Exception: {e.Message} on {e.ParamName}";
+        }
+
+        #endregion
+
+        #region InputFileExtended
+        void OnError(ArgumentException e)
+        {
+            Messages = $"Exception: {e.Message} on {e.ParamName}";
+        }
+
+        void OnSelect(FileUploadEventArgs e)
+        {
+            FileHandler.Add(e.File);
+            Messages = $"File {e.File.Name} chosen. Total size {e.File.Size}.";
+        }
+        #endregion
+
+        async Task Upload()
+        {
+            Messages = $"Start upload simulating ...";
+            bool result = await FileHandler.UploadAsync<bool>("files");
+            await Task.Delay(3000);
+            Messages = $"Files Upload with result {result}";
+        }
+    }
+}
