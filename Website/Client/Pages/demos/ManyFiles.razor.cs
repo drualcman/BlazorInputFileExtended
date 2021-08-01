2820e7f0078cd282cc7273c5fc321d1383f8f448
Sergi Ortiz Gómez <drualcman@msn.com>
2021-08-01 20:15:46 +0800

    Add new demo with multiple input file

---
+++ Website/Client/Pages/demos/ManyFiles.razor.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BlazorInputFileExtended;
+
+namespace Website.Client.Pages.demos
+{
+    public partial class ManyFiles
+    {
+        List<FileUploadContent> Files = new List<FileUploadContent>();
+        string Messages;
+
+        void GetFile(FileUploadEventArgs e)
+        {
+            Files.Add(e.File);
+            if (Files.Count > 6) Files.RemoveAt(0);
+            Messages = $"Files selected: {Files.Count}";
+        }
+
+        async Task Upload()
+        {
+            int c = Files.Count;
+            if (c> 0)
+            {
+                for (int i = 0; i < c; i++)
+                {
+                    Messages = $"Simulate Uploading file {Files[i].Name}";
+                    StateHasChanged();
+                    await Task.Delay(750);
+                }
+                Messages = $"Files uploaded {c}";                
+                StateHasChanged();
+            }
+            else
+            {
+                Messages = "No files to upload";
+            }
+        }
+    }
+}
